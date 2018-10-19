5b0a5e14e167234d1e793f7d723ca3bf032cf679
Wei Liu <newway.liu@gmail.com>
2018-10-19 22:01:28 +0100

    face lock with openCV and Microsoft Cognitive API

--- facelock/facelock.py
+++ facelock/facelock.py
@@ -17,7 +17,7 @@ from time import sleep
 from urllib.parse import urlparse
 from pid.decorator import pidfile
 from PIL import Image
-
+from facelock.face_api import save_reference_face, face_verify
 
 
 class FaceLock(object):
@@ -81,8 +81,9 @@ class FaceLock(object):
             self.model['time'] = time.time()
             self.model['image'] = ref_image_url
             log.debug('saving model:', self.model)
-            # pickle.dump(self.model, open(self.config['DEFAULT']['MODEL_FILE'], "wb"))
+
             return response.status_code
+
         except requests.ConnectionError as e:
             log.error("failed to connect to %s: s" % (self.config['DEFAULT']['URL']+self.face_detect_endpoint, e))
 
@@ -102,6 +103,8 @@ class FaceLock(object):
         """
         try:
             self.model = self.load_model()
+            return_dict = dict()
+
             payload = self.read_image(image_data)
             header = self.face_detect_headers.copy()
             header['Content-Type'] = 'application/octet-stream'
@@ -109,10 +112,7 @@ class FaceLock(object):
                                      data=payload,
                                      headers=header)
 
-            # no face detected
-            if response.content == b'[]':
-                log.warning('no face detected.')
-                return None, None
+            response.raise_for_status()
 
             log.debug('detect response.content={}'.format(json.loads(response.content)))
 
@@ -120,16 +120,20 @@ class FaceLock(object):
             response_dict = json.loads(response.content)[0]
 
             data_dict['faceId1'] = response_dict['faceId']
+            return_dict['faceId'] = response_dict['faceId']
+            return_dict['faceRectangle'] = response_dict['faceRectangle']
             # reference faceId
             data_dict['faceId2'] = self.model['faceId']
 
             response_verify = requests.post(url=self.config['DEFAULT']['URL']+self.face_verify_endpoint,
                                             data=json.dumps(data_dict),
                                             headers=self.face_verify_headers)
-
             log.debug('verify response.content={}'.format(json.loads(response_verify.content)))
+            response_verify.raise_for_status()
+
             response_dict2 = json.loads(response_verify.content)
-            return response_dict, response_dict2
+            return_dict['confidence'] = response_dict2['confidence']
+            return return_dict
 
         except requests.exceptions.RequestException as e:
             log.error("Error: {1}".format(e.strerror))
@@ -177,7 +181,10 @@ class FaceLock(object):
             raise Exception('Unsupported OS platform: %s' % os_type)
 
     def run(self, delay_seconds, sleep_seconds, display, always):
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
         video_capture = cv2.VideoCapture(0)
+        video_capture.set(cv2.CAP_PROP_FPS, 10)
         frame_rate = video_capture.get(5) #frame rate
         trigger = int(delay_seconds / sleep_seconds)
         counter = 0
@@ -191,109 +198,55 @@ class FaceLock(object):
 
             frame_id = video_capture.get(1) # current frame number
             ret, frame = video_capture.read()
-            log.debug('frame rate={}, frame id={}'.format(frame_rate, frame_id))
+            # log.debug('frame rate={}, frame id={}'.format(frame_rate, frame_id))
             frame = cv2.resize(frame, (300, 200))
-            img = Image.fromarray(frame, 'RGB')
-            img_byte_arr = io.BytesIO()
-            img.save(img_byte_arr, format='JPEG')
-            # img.save('webcam_capture.jpg', format='JPEG')
-            img_byte_arr = img_byte_arr.getvalue()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(25, 25)
+            )
 
-            detect_response, verify_response = self.face_verify(img_byte_arr)
-
-            if detect_response is None:
+            # use openCV's face detection, if there is no face, we don't need to waste
+            # API calls
+            if len(faces) < 1:
                 counter += 1
                 log.info('No face detected, counter = {}'.format(counter))
-
-                if counter > trigger:
-                    self.lock_screen()
-
-                t2 = time.time()
-                sleep_time = max(0, sleep_seconds - (t2 - t1))
-                time.sleep(sleep_time)  # Sleep for x second, discounting the running time of the program
-                continue
-
-            face_rectangle = detect_response['faceRectangle']
-            confidence = verify_response['confidence']
-            if confidence < float(self.config['DEFAULT']['RECOGNITION_THRESHOLD']):
-                counter += 1
-                log.info('Wrong face detected, counter = {}'.format(counter))
             else:
-                counter = 0
-                log.info('Your face detected, reset counter to {}'.format(counter))
+                img = Image.fromarray(frame, 'RGB')
+                img_byte_arr = io.BytesIO()
+                img.save(img_byte_arr, format='JPEG')
+                img_byte_arr = img_byte_arr.getvalue()
 
-            if display and confidence >= float(self.config['DEFAULT']['RECOGNITION_THRESHOLD']):
-                x = face_rectangle['left']
-                y = face_rectangle['top']
-                w = face_rectangle['width']
-                h = face_rectangle['height']
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                response = self.face_verify(img_byte_arr)
+
+                face_rectangle = response['faceRectangle']
+                confidence = response['confidence']
+                if confidence < float(self.config['DEFAULT']['RECOGNITION_THRESHOLD']):
+                    counter += 1
+                    log.info('Wrong face detected, counter = {}'.format(counter))
+                else:
+                    counter = 0
+                    log.info('Your face detected, reset counter to {}'.format(counter))
+
+                if display and confidence >= float(self.config['DEFAULT']['RECOGNITION_THRESHOLD']):
+                    x = face_rectangle['left']
+                    y = face_rectangle['top']
+                    w = face_rectangle['width']
+                    h = face_rectangle['height']
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
             cv2.imshow('Face Recognition', frame)
             cv2.waitKey(200)
 
             if counter > trigger:
+                counter = 0
                 self.lock_screen()
 
             t2 = time.time()
             sleep_time = max(0, sleep_seconds - (t2 - t1))
             time.sleep(sleep_time)  # Sleep for x second, discounting the running time of the program
-
-    # def run_cv(self, delay_seconds, sleep_seconds, display, always):
-    #     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-    #     log.basicConfig(level=log.DEBUG)
-    #
-    #     video_capture = cv2.VideoCapture(0)
-    #     anterior = 0
-    #     counter = 0
-    #     trigger = int(delay_seconds/sleep_seconds)
-    #
-    #     while True:
-    #         t1 = time.time()
-    #         if not video_capture.isOpened():
-    #             print('Unable to load camera.')
-    #             sleep(3)
-    #             pass
-    #
-    #         ret, frame = video_capture.read()
-    #         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #
-    #         faces = face_cascade.detectMultiScale(
-    #             gray,
-    #             scaleFactor=1.1,
-    #             minNeighbors=5,
-    #             minSize=(50, 50)
-    #         )
-    #
-    #         if len(faces) < 1:
-    #             counter += 1
-    #             log.info("[%s] no face detected, counter=%d" % (dt.datetime.now(), counter))
-    #
-    #             if counter > trigger:
-    #                 self.lock_screen()
-    #
-    #                 if not always:
-    #                     video_capture.release()
-    #                     cv2.destroyAllWindows()
-    #                     sys.exit()
-    #
-    #         if anterior != len(faces):
-    #             anterior = len(faces)
-    #             log.info("[%s] faces: %d" % (dt.datetime.now(), len(faces)))
-    #             counter = 0
-    #
-    #         if display:
-    #             # Draw a rectangle around the faces
-    #             for (x, y, w, h) in faces:
-    #                 cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    #             # Display the resulting frame
-    #             frame = cv2.resize(frame, (300, 200))
-    #             cv2.imshow('Face Detection', frame)
-    #             cv2.waitKey(200)
-    #
-    #         t2 = time.time()
-    #         sleep_time = max(0, sleep_seconds-(t2-t1))
-    #         time.sleep(sleep_time)  # Sleep for x second, discounting the running time of the program
 
 
 @click.group()
@@ -304,7 +257,7 @@ def main():
 @main.command()
 @click.option('-t', '--trigger-seconds', default=30,
               help='activate command after this many seconds without detecting your face')
-@click.option('--sleep-seconds', help='sleep every this many seconds', default=6)
+@click.option('--sleep-seconds', help='sleep every this many seconds', default=1)
 @click.option('--display', help='display a webcam window', is_flag=True, default=False)
 @click.option('--always', help='do not exist after screen is locked', is_flag=True, default=False)
 def fit(trigger_seconds, sleep_seconds, display, always):
